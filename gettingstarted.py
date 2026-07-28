2ae092a9b70bf9eaa2eba8deacd7392d65d6d24d
ZachOG10 <aondowasezachariahog@gmail.com>
2026-07-28 15:10:09 +0100

    gettingstarted.py

---
+++ gettingstarted.py
@@ -0,0 +1,8 @@
+## what is programing?
+Programing is simply the skill of talking to a machine that does exactly what you say, in exactly the order you say it, without ever guessing what you meant.
+##key concepts:
+Programing: Giving a computer a list of instructions to reach a given goal.
+Sequencing (order matters): The exact order instructions run in. Change the order, and the outcome changes.
+Percision (zero Guesswork): Eliminating all room for interpretation.
+State (The Machine's Memory): The current status of something at a given moment.
+
