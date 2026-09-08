a4b89442e81cbca8848b12e06a922e433d2d020a
ZachOG10 <aondowasezachariahog@gmail.com>
2026-09-08 11:41:40 +0100

    Computationalthinking.py

---
+++ Computationalthinking.py
@@ -0,0 +1,66 @@
+"###COMPUTATIONALTHINKING
+
+****Computational thinking is a systematic way of approaching a problem and solving it in a way
+that is logical, repeatable, efficient, understandable, and executable by the computer.
+
+"###The four concepts of computational thinking are:
+1. Decomposition: This is the process of breaking down complex problems into smaller
+and understandable parts.
+2. Pattern recognition: This is the method of understanding the parts of the problem
+that repeats itself.
+3. Abstraction: This is the process of focusing on the important information while
+ignoring unnecessary detail.
+4. Algorithmic thinking is the process of knowing the steps required to solve a problem
+and in what order.
+
+"###Computational Thinking is:
+✅ Breaking problems down
+✅ Looking for patterns
+✅ Identifying what matters
+✅ Creating logical steps
+✅ Generalizing solutions
+✅ Thinking about edge cases
+✅ Testing your assumptions
+✅ Improving solutions
+✅ Explaining your reasoning.
+
+"###The progression you should aim for:
+
+"###As a beginner, you might think:
+
+***How do I write this code?
+
+"###Then progress to:
+
+***What steps do I need?
+
+"###Then:
+
+***What's the pattern?
+
+"###Then:
+
+***Can I generalize this?
+
+"###Then:
+
+***Can I make this solution simpler?
+
+"###Then eventually:
+
+****What's the most appropriate and efficient way to solve this problem?
+"###That progression is a big part of becoming a strong programmer.
+
+"###Your ultimate goal:
+***As you're learning Python, don't make your primary goal:
+
+"I want to memorize Python."
+
+****Make your goal:
+
+"I want to become good at solving problems, and I'll use Python to express my solutions."
+
+That's a much stronger mindset.
+
+Eventually, when you encounter a new programming language,
+you won't feel like you're starting from zero.
