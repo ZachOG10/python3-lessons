bc641cdcfb658a423fdf874edb7ac54f8eb3b034
ZachOG10 <aondowasezachariahog@gmail.com>
2026-09-09 00:20:08 +0100

    name.py

---
+++ name.py
@@ -0,0 +1,13 @@
+###Changing case in a string whith Methods
+##A method is an action that python can perform on a piece of data.
+##The dot(.) after the variable (name)tells python to allow the title()
+##method to act on variable (name).
+# name = "ada lovelace"
+# print(name.title())  
+
+###string concatenation
+
+first_name = "Zachariah"
+last_name = "Aondowase"
+full_name = first_name + ' ' + last_name
+print(full_name) 
---
+++ variable.py
@@ -0,0 +1,6 @@
+"###This is how to declear a variable
+
+student_name = "Zachariah"
+my_name = "Abigail"
+print(my_name)
+print(student_name) 
