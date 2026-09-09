05cca4d511cdfb5d58c5d05d824d4e4bf780fcdb
ZachOG10 <aondowasezachariahog@gmail.com>
2026-09-09 10:37:39 +0100

    variable.py

--- variable.py
+++ variable.py
@@ -1,6 +1,9 @@
-"###This is how to declear a variable
-
+###This is how to declear a variable
+###Every variable holds a value, which is the information associated whith that variable.
+###A traceback is a record of where interpreter ran into a problem while trying
+##to execute a progragm. 
 student_name = "Zachariah"
 my_name = "Abigail"
 print(my_name)
 print(student_name) 
+
