a42f8516c78d20a86cd24380ed85e1a23e2c0841
ZachOG10 <aondowasezachariahog@gmail.com>
2026-09-09 14:35:02 +0100

    best_number.py

---
+++ best_number.py
@@ -0,0 +1,5 @@
+##This program stores an integer in a vareable and stransforms it to a string
+## to concatenate it with other strings. 
+my_number = 10
+message = "My favorite number is " + str(my_number)
+print(message) 
