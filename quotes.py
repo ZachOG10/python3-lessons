c007553dd5c311cbda50a08aa98ef00b862c1a54
ZachOG10 <aondowasezachariahog@gmail.com>
2026-09-09 11:55:46 +0100

    quotes.py

---
+++ quotes.py
@@ -0,0 +1,7 @@
+###This illustrates how to use quotes in strings. 
+star_boy = "Wizkid"
+print(f'{star_boy} once said "Look sharp stop sleeping."') 
+
+famous_person = star_boy
+message = f'{famous_person} once said "Look sharp stop sleeping."'
+print(message) 
