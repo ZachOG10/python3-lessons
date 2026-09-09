52d238aa799f347fbe2396a77303d5e38fee0ffe
ZachOG10 <aondowasezachariahog@gmail.com>
2026-09-09 11:17:47 +0100

    name.py

--- name.py
+++ name.py
@@ -2,12 +2,21 @@
 ##A method is an action that python can perform on a piece of data.
 ##The dot(.) after the variable (name)tells python to allow the title()
 ##method to act on variable (name).
-# name = "ada lovelace"
-# print(name.title())  
+name = "ada lovelace"
+print(name.title())  
 
 ###string concatenation
 
 first_name = "Zachariah"
 last_name = "Aondowase"
 full_name = first_name + ' ' + last_name
-print(full_name) 
+message = "Hello" + " " + full_name + "!"
+print(message) 
+
+###To remove whitespace from a string, use the rstrip(), lstrip(), or strip() methods.
+###To remove the whitespaces permanently, you have to assign the stripped value back
+##into the variable.
+###A syntax error occurs when python encounters code that does not follow the proper
+###stuctural rules of the language. 
+greetings = "Good morning "
+print(greetings.rstrip())
