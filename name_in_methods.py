9893d198e964504955be33f4e57b9597a7438ffa
ZachOG10 <aondowasezachariahog@gmail.com>
2026-09-09 11:54:23 +0100

    name_in_methods.py

---
+++ name_in_methods.py
@@ -0,0 +1,11 @@
+###This prints the names in defferent cases.
+Student_name = "Michael"
+print(Student_name.lower())
+boy_name = "Joseph" 
+print(boy_name.upper())
+his_name = "mark"
+print(his_name.title()) 
+name = "  Michael  "
+print(name.lstrip())
+print(name.rstrip())
+print(name.strip()) 
