92918806151a3e1490b0b2db658e30a340b931eb
ZachOG10 <aondowasezachariahog@gmail.com>
2026-09-10 12:02:37 +0100

    looping_list.py

---
+++ looping_lists.py
@@ -0,0 +1,4 @@
+pizza_names = ['domino','sudo','beef','chicken']
+for pizza in pizza_names:
+    print("I like pepperino" + " " + pizza.title() + " " + "pizza!")
+    print("I hope to have more of" + " " + pizza.title() + " " + "pizza soon" + "\n")
